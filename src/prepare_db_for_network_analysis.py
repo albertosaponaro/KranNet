4aed4f43106df93e76bacacd5dc7bf4f3f59ddc2
Alberto Saponaro <saponaro@belwue.de>
2024-06-18 13:46:53 +0200

    small fixes

--- src/prepare_db_for_network_analysis.py
+++ src/prepare_db_for_network_analysis.py
@@ -1,37 +1,10 @@
-import utils
-import sql_utils
+import env, utils, sql_utils
 import pandas as pd
-from tqdm import tqdm 
-
-def create_tables():
-    # SQL queries to create the tables
-    create_meeting_table = """
-    CREATE TABLE IF NOT EXISTS meeting_table (
-        meeting_id SERIAL PRIMARY KEY,
-        title VARCHAR(100) NOT NULL,
-        year INT NOT NULL
-    );
-    """
-
-    create_speaker_table = """
-    CREATE TABLE IF NOT EXISTS speaker_table (
-        speaker_id SERIAL PRIMARY KEY,
-        meeting_id INT NOT NULL,
-        speaker_name VARCHAR(50) NOT NULL,
-        interventions SMALLINT NOT NULL
-    );
-    """
-
-    # Execute the transition function to create the tables
-    sql_utils.transition(create_meeting_table)
-    sql_utils.transition(create_speaker_table)
-    print('Tables created successfully.')
+from tqdm import tqdm
+import pgdumplib
 
 
 def main():
-    # Drop DB
-    sql_utils.drop_db('krannet')
-
     input_file, output_file = utils.parse_arguments()
 
     # Load meeting infos from cache
@@ -43,7 +16,7 @@ def main():
     sql_utils.create_db('krannet')
 
     # Create tables
-    create_tables()
+    env.create_krannet_tables()
 
     numb_of_speakers = 0 # sanity check
 
@@ -55,8 +28,7 @@ def main():
         params = (row.title, row.year)
         sql_utils.transition(query, params, verbose=False)
 
-        # TODO - if the transition fails we shouls skeep inserting the meeting --> too much work maybe just delete restriction of 100 CHARS!!!
-
+        
         for speaker, intervention in row.speakers.items():
             
             # Insert speakers into the speakers table
@@ -70,10 +42,13 @@ def main():
     # Show head of the two tables
     meeting_df = sql_utils.fetch_table_data(table_name='meeting_table')
     speaker_df = sql_utils.fetch_table_data(table_name='speaker_table')
-    print('\nPreview DataFrame head... \n', meeting_df.head(), '\n', 'Number of meetings:', len(meeting_df), 'of', len(df), '\n') # 564 of 667
+    print('\nPreview DataFrame head... \n', meeting_df.head(), '\n', 'Number of meetings:', len(meeting_df), 'of', len(df), '\n')
     print('\nPreview DataFrame head... \n', speaker_df.head(), '\n', 'Number of meetings:', len(speaker_df), 'of', numb_of_speakers, '\n')
 
-    # TODO - Save database or not?! --> Save but how (ISSUES with Postgress and VENV)
+    # Save tables as CSV
+    print('\nBackup tables as CSV files')
+    meeting_df.to_csv(f'{output_file}_meeting_table.csv', sep='\t')
+    speaker_df.to_csv(f'{output_file}_speaker_table.csv', sep='\t')
 
     # Drop DB
     sql_utils.drop_db('krannet')
